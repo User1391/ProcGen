07a1a9f08ea69aa636c591f31463d678a4135abb
Maxwell Penders <max.penders@gmail.com>
2023-04-26 21:30:32 -0400

    Added Folder Structure

---
+++ programs/mazes/draw_map.py
@@ -0,0 +1,20 @@
+from PIL import Image
+def draw(two_d_list):
+    for i in range(len(two_d_list)):
+        for j in range(len(two_d_list[0])):
+            if two_d_list[i][j]:
+                print('#', end='')
+            else:
+                print(' ', end='')
+        print('')
+
+def draw_img(two_d_list):
+    # from https://stackoverflow.com/questions/20304438/how-can-i-use-the-python-imaging-library-to-create-a-bitmap
+    img = Image.new('RGB', (len(two_d_list), len(two_d_list[0])), "black")
+    pixels = img.load()
+    for i in range(len(two_d_list)):
+        for j in range(len(two_d_list[0])):
+            if two_d_list[i][j]:
+                pixels[i,j] = (255, 255, 255)
+
+    img.show()
