7763a45d486571402b1663e4122b44c7f53f6382
Maxwell Penders <max.penders@gmail.com>
2023-07-16 15:53:29 -0400

    Started work on more

--- programs/caves/draw_map.py
+++ programs/caves/draw_map.py
@@ -18,3 +18,13 @@ def draw_img(two_d_list):
                 pixels[i,j] = (255, 255, 255)
 
     img.show()
+
+def draw_img_color(two_d_list):
+    img = Image.new('RGB', (len(two_d_list), len(two_d_list[0])), "black")
+    pixels = img.load()
+    for i in range(len(two_d_list)):
+        for j in range(len(two_d_list[0])):
+            #print(two_d_list[i][j], "pixel")
+            pixels[i, j] = two_d_list[i][j]
+
+    img.show()
